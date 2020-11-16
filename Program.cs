44ee90ae708050e8434c3b681f48e50379e4c5f6
Opilane TTHK <opilane@tthk.ee>
2020-11-16 15:05:36 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Fileproperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+            string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+            list<string> lines = new List
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+                //get file objects
+                var fileData = new FileInfo(file);
+                Console.WriteLine(fileData);
+                string fileName = fileData.Name;
+                string FileDirectory = fileData.DirectoryName;
+                long fileSize = fileData.Length;
+
+                Console.WriteLine($"File name: {fileName}; location: {FileDirectory}; Size: {fileSize}");
+                string line = ($"File name: {fileName}; location: {FileDirectory}; Size: {fileSize}");
+            }
+
+        }
+    }
+}
---
+++ list.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fileproperties
+{
+    internal class list<T>
+    {
+        internal void Add(string line)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
